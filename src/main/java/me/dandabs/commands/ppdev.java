7477a40f9e54f2ba4e7504b2812f42162b92ddc0
Sebastian Nesci <hydro@cloudcraftmc.org.uk>
2020-02-21 14:14:10 -0500

    This Sends A Message When The Player Does /ppdev

--- src/main/java/me/dandabs/commands/ppdev.java
+++ src/main/java/me/dandabs/commands/ppdev.java
@@ -17,7 +17,7 @@ public class ppdev implements CommandExecutor {
 
             File langFile = new File("cloudconf", "lang.yml");
             YamlConfiguration langConfig = YamlConfiguration.loadConfiguration(langFile);
-player.sendMessage("Developed By Sebastian Nesci And Daniel Adams")
+player.sendMessage("Developed By Sebastian Nesci And Olavi Koskinen ")
             File userFile = new File("cloudconf" + File.separator + "users", player.getUniqueId().toString() + ".yml");
             YamlConfiguration userConfig = YamlConfiguration.loadConfiguration(userFile);
 
