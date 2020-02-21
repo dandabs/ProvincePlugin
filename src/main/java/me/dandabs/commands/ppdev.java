672004e9fb6673a5ed48f59fd0e2608b1c0d3f8a
Sebastian Nesci <hydro@cloudcraftmc.org.uk>
2020-02-21 14:05:36 -0500

    This Sends A Message When The Player Does /ppdev

---
+++ src/main/java/me/dandabs/commands/ppdev.java
@@ -0,0 +1,26 @@
+package me.dandabs.commands;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+
+public class ppdev implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            File langFile = new File("cloudconf", "lang.yml");
+            YamlConfiguration langConfig = YamlConfiguration.loadConfiguration(langFile);
+player.sendMessage("Developed By Sebastian Nesci And Daniel Adams")
+            File userFile = new File("cloudconf" + File.separator + "users", player.getUniqueId().toString() + ".yml");
+            YamlConfiguration userConfig = YamlConfiguration.loadConfiguration(userFile);
+
+
+            return true;
+        }}}
