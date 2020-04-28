39ba2c747031bb4b98301bc941e5b1d1dfd14a94
Daniel Adams <danstanswinxp@gmail.com>
2020-04-28 14:31:32 +0100

    Regional points system

--- src/main/java/me/dandabs/listeners/PlayerLeave.java
+++ src/main/java/me/dandabs/listeners/PlayerLeave.java
@@ -1,14 +1,46 @@
 package me.dandabs.listeners;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.io.File;
 
 public class PlayerLeave implements Listener {
 
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent event) {
+
+        int points = 0;
+
+        Player player = (Player) event.getPlayer();
+
+        File langFile = new File("cloudconf", "lang.yml");
+        YamlConfiguration langConfig = YamlConfiguration.loadConfiguration(langFile);
+
+        File userFile = new File("cloudconf" + File.separator + "users", player.getUniqueId().toString() + ".yml");
+        YamlConfiguration userConfig = YamlConfiguration.loadConfiguration(userFile);
+
+        int placed = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_blocks_placed%"));
+        int broken = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_blocks_broken%"));
+        int killed = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_players_killed%"));
+        int died = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_deaths%"));
+        int crafted = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_crafted_items%"));
+        int trades = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_villager_trades%"));
+        int mobs = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_mobs_killed%"));
+
+        points += placed / 200;
+        points -= broken / 100;
+        points += killed * 5;
+        points -= died * 5;
+        points += crafted / 2;
+        points += trades * 3;
+        points += mobs * 2;
+
+        userConfig.set("player.points", points);
 
         event.setQuitMessage(" §6{§c-§6} §6" + event.getPlayer().getName());
 
--- src/main/java/me/dandabs/utilities/RegionPoints.java
+++ src/main/java/me/dandabs/utilities/RegionPoints.java
@@ -1,7 +1,5 @@
 package me.dandabs.utilities;
 
-import me.clip.placeholderapi.PlaceholderAPI;
-import me.dandabs.Plugin;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -37,31 +35,7 @@ public class RegionPoints {
 
                 OfflinePlayer player = Bukkit.getOfflinePlayer(UUID.fromString(userConfig.getString("player.uuid")));
 
-                if (player.isOnline()) {
-
-                } else {
-
-                    if (!Plugin.getPermissions().playerHas("world", player, "provinceplugin.meta.staff")) {
-
-                        int placed = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_blocks_placed%"));
-                        int broken = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_blocks_broken%"));
-                        int killed = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_players_killed%"));
-                        int died = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_deaths%"));
-                        int crafted = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_crafted_items%"));
-                        int trades = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_villager_trades%"));
-                        int mobs = Integer.parseInt(PlaceholderAPI.setPlaceholders(player, "%statz_mobs_killed%"));
-
-                        points += placed;
-                        points -= broken;
-                        points += killed * 5;
-                        points -= died * 5;
-                        points += crafted / 2;
-                        points += trades * 3;
-                        points += mobs * 2;
-
-                    }
-
-                }
+                points += userConfig.getInt("player.points");
 
             }
 
